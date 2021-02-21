f92deef49e2d4cf687bf25850a7e5361e98d3b4e
korezonzi <korezonzip@gmail.com>
2021-02-21 16:07:15 +0900

    creating now

---
+++ ch3/my-first-node-js/04_analiticsPrefPeople.js
@@ -0,0 +1,6 @@
+'use strict';
+const fs = require('fs');
+const readline = require('readline');
+const rs = fs.createReadStream('./00_popu-pref.csv');
+const rl = readline.createInterface({input: rs, output: {}});
+rl.on('line', lineString999)
