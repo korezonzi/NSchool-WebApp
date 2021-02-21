83d9884b4f02874896b99ef9e667ba8f1ab7ae59
fuma_tsuchiya <fuma.tsuchiya@nextbeat.net>
2021-02-22 01:06:46 +0900

    output filter csv data by year and column

--- ch3/my-first-node-js/04_analiticsPrefPeople.js
+++ ch3/my-first-node-js/04_analiticsPrefPeople.js
@@ -1,6 +1,20 @@
 'use strict';
-const fs = require('fs');
+const fs       = require('fs');
 const readline = require('readline');
-const rs = fs.createReadStream('./00_popu-pref.csv');
-const rl = readline.createInterface({input: rs, output: {}});
-rl.on('line', lineString999)
+const rs       = fs.createReadStream('ch3/my-first-node-js/00_popu-pref.csv');
+const rl       = readline.createInterface({input: rs, output: {}});
+rl.on('line', lineString => {
+  const columns     = lineString.split(',');
+  const year        = parseInt(columns[0]);
+  const pref        = columns[2];
+  const popu        = parseInt(columns[4]);
+  const TARGET_YEAR = [1995, 2000];
+
+  if(TARGET_YEAR.includes(year)) {
+    console.log(year + '/ ' + pref + '/ ' + popu);
+  }
+});
+/* これでもいける??
+rl.line = () => {
+  console.log(rl);
+} */
