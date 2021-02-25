008267e96a841e1107d6ec633744f69ac51c2dbb
korezonzi <korezonzip@gmail.com>
2021-02-26 02:28:37 +0900

    use Map by refactring

--- ch3/my-first-node-js/04_analiticsPrefPeople.js
+++ ch3/my-first-node-js/04_analiticsPrefPeople.js
@@ -3,16 +3,34 @@ const fs       = require('fs');
 const readline = require('readline');
 const rs       = fs.createReadStream('ch3/my-first-node-js/00_popu-pref.csv');
 const rl       = readline.createInterface({input: rs, output: {}});
+const prefDataMap = new Map(); //(pref -> 集計データのオブジェクト)
 rl.on('line', lineString => {
   const columns     = lineString.split(',');
   const year        = parseInt(columns[0]);
   const pref        = columns[2];
   const popu        = parseInt(columns[4]);
-  const TARGET_YEAR = [1995, 2000];
+  const TARGET_YEAR = [2000,2015];
 
   if(TARGET_YEAR.includes(year)) {
-    console.log(year + '/ ' + pref + '/ ' + popu);
+    let value = prefDataMap.get(pref);
+    if(!value) {
+      value = {
+        popu10: 0,
+        pupu15: 0,
+        change: null
+      };
+    }
+    if(year === 2010) {
+      value.popu10 = popu;
+    }
+    if(year === 2015) {
+      value.popu15 = popu;
+    }
+    prefDataMap.set(pref, value);
   }
+});
+rl.on('close', () => {
+  console.log(prefDataMap);
 });
 /* これでもいける??
 rl.line = () => {
