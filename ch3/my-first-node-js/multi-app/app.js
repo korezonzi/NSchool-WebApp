6ad9f428dda58b5aec31547be222a14eda6854d3
korezonzi <korezonzip@gmail.com>
2021-02-28 18:09:57 +0900

    練習:npmパッケージーの作成(配列の掛け算)

---
+++ ch3/my-first-node-js/multi-app/app.js
@@ -0,0 +1,3 @@
+'use strict';
+const x = require('multi');
+console.log(x.multi([1,2,3]));
---
+++ ch3/my-first-node-js/multi/index.js
@@ -0,0 +1,10 @@
+'use strict';
+function multi(numbers) {
+  let result = 1;
+  for(let num of numbers) {
+    result *= num
+  }
+  return result;
+}
+
+module.exports = {multi};
